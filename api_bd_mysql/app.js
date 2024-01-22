db67188f9efa45f58ff2ef66cb2051dde2822ebc
Iohana Maria <122891414+IohanaViterbino@users.noreply.github.com>
2024-01-22 17:09:37 -0300

    aula 22/01

--- api_bd_mysql/app.js
+++ api_bd_mysql/app.js
@@ -6,12 +6,15 @@ const port = 3000
 
 app.use(cors())
 app.use(express.json())
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
 app.use('/alunos', router)
 
 
 app.get('/', (req, res) =>{
-    res.send('Hello')
-})
+    // res.send('Hello')
+    res.render('index')
+})  
 
 app.listen(port, () => {
     console.log(`Aplicação rodando em http://localhost:${port}`)
--- api_bd_mysql/controller/alunoController.js
+++ api_bd_mysql/controller/alunoController.js
@@ -1,57 +1,69 @@
 const db = require('../database/connection')
 
 const alunoControler = {
-
-    novoAluno:(req, res)=> {
-        const {matricula, nome, email, data_nascimento, turma, fone, sexo} = req.body
-
-        db.insert({matricula, nome, email, data_nascimento, turma, fone, sexo}).table("alunos").then(data =>{
-            console.log(data)
-            res.json({message: 'aluno salvo com sucesso'})
-        }).catch(error =>{
-            console.log(error)
+    //render do front
+    formCadastro: (req, res)=>{
+        res.render('cadastroAluno')
+    },
+    formEditarAluno: (req,res)=>{
+        const id = req.params
+        db.select("*").table("alunos").where({matricula: id.matricula}).then(data =>{ 
+            res.render('editarAluno', {data})
+        }).catch(error=>{
+            res.json(error)
         })
     },
     listarAlunos: (req, res)=> {
         db.select("*").table("alunos").then(data =>{ 
             //data equivale aos alunos listados do bd
-            console.log(data)
-            res.json(data)
+            // res.json(data)
+            res.render('listarAlunos', {alunos:data})
         }).catch(error=>{
-            console.log(error)
+            res.json(error)
+        })
+    },
+    novoAluno:(req, res)=> {
+        const {matricula, nome, email, data_nascimento, turma, fone, sexo} = req.body
+
+        db.insert({matricula, nome, email, data_nascimento, turma, fone, sexo}).table("alunos").then(data =>{
+            res.json({message: 'aluno salvo com sucesso'})
+        }).catch(error =>{
+            res.json(error)
         })
     },
     buscarAlunoM: (req, res)=> {
         const id = req.params
-        console.log(id)
         db.select("*").table("alunos").where({matricula: id.matricula}).then(data =>{ 
-            console.log(data)
             res.json(data)
         }).catch(error=>{
-            console.log(error)
+            res.json(error)
         })
     },
     buscarAlunoT: (req, res)=> {
         const id = req.params
-        console.log(id)
         db.select("*").table("alunos").where({turma: id.turma}).then(data =>{ 
-            console.log(data)
             res.json(data)
         }).catch(error=>{
-            console.log(error)
+            res.json(error)
         })
     },
     deletarAluno: (req, res)=> {
         const id = req.params
-        console.log(id)
         db.delete("*").table("alunos").where({matricula: id.matricula}).then(data =>{ 
-            console.log('apagou')
             res.json({message: 'aluno apagado com sucesso'})
         }).catch(error=>{
-            console.log(error)
+            res.json(error)
+        })
+    },
+    editarAluno:(req, res) =>{
+        const id = req.params
+        const {nome, email, data_nascimento, turma, fone, sexo} = req.body
+        db.where({matricula:id.matricula}).update({nome, email, data_nascimento, turma, fone, sexo}).table("alunos").then(data =>{ 
+            res.json({message: 'registro do aluno atualzado com sucesso'})
+        }).catch(error=>{
+            res.json(error)
         })
     }
-
 }
 
 module.exports = alunoControler
