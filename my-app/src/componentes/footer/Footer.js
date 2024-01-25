2fddf9d5766c0de50689c0b8fa4ef0a3641a8915
Iohana <iohana.ma31@gmail.com>
2024-01-25 14:06:20 -0300

    24/01 primeiro react

---
+++ my-app/src/componentes/footer/Footer.js
@@ -0,0 +1,11 @@
+
+
+function Footer() {
+    return(
+        <p>
+            editions by <a href="https://github.com/IohanaViterbino">Iohana Viterbino</a>
+        </p>
+    );
+}
+
+export default Footer;
